bc7ddac0aede940f677746782044e9c0b9fd1ee5
001andisa <andisuprianto29@gmail.com>
2019-09-25 17:25:39 +0700

    side bar fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
             <div class="header-body text-center mt-7 mb-7">
                 <div class="row justify-content-center">
                     <div class="col-lg-5 col-md-6">
-                        <h1 class="text-white">{{ __('Welcome to Argon Dashboard FREE Laravel Live Preview.') }}</h1>
+                        <h1 class="text-white">{{ __('Sistem Pemerintahan Berbasis Elektronik KEMENPAN Republik Indonesia') }}</h1>
                     </div>
                 </div>
             </div>
@@ -17,6 +17,5 @@
             </svg>
         </div>
     </div>
-
     <div class="container mt--10 pb-5"></div>
 @endsection
