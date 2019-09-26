b9f7f0fb35bff0d789754b76961dd05861ef8431
Fadhil Yori Hibatullah <fadhil4812@gmail.com>
2019-09-26 16:59:03 +0700

    Add the Stage database seeder and Model.
    Signed-off-by: Fadhil Yori Hibatullah <fadhil4812@gmail.com>

---
+++ database/seeds/StagesTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Stage;
+use Illuminate\Database\Seeder;
+
+class StagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $stages = [
+            [
+                'name' => 'Evaluasi Mandiri'
+            ],
+            [
+                'name' => 'Evaluasi Dokumen'
+            ],
+            [
+                'name' => 'Wawancara'
+            ],
+            [
+                'name' => 'Observasi Lapangan'
+            ],
+            [
+                'name' => 'Adjustment Final'
+            ],
+        ];
+
+        Stage::query()->create($stages);
+    }
+}
